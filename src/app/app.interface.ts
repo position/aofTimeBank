0ba1ab2c104df94d2b342f74c8aa5c0a49a0e1e1
Benjamin Choi <positionfov@gmail.com>
2017-03-15 18:35:42 +0900

    소스커밋4

---
+++ src/app/app.interface.ts
@@ -0,0 +1,11 @@
+export interface iSprite {
+    name: string ;
+    width: number;
+    height: number;
+    dx: number;
+    dy: number;
+    image: any;
+    numberOfFrames: number;
+    ticksPerFrame: number;
+    isSprite: boolean;
+}
